5eac3b4a58b62d9538ea47e891dc0230d69a099e
sbartholomew <bartmnz@gmail.com>
2016-06-30 16:31:07 -0400

    implemented math-like function things

--- src/fdr.py
+++ src/fdr.py
@@ -8,6 +8,7 @@ from socket import*
 import sys
 import select
 from _socket import AF_INET
+from cmath import sqrt
 
 
 class Make_server():
@@ -15,7 +16,7 @@ class Make_server():
         self.address = ('localhost', port)
         self.server_socket = socket(AF_INET, SOCK_DGRAM)
         self.server_socket.bind(address)
-        self.hex_ret = None
+        self.rValue = None
         self.recv_data = None
         self.quit_flag = False
     
@@ -23,7 +24,10 @@ class Make_server():
         while(not self.quit_flag):
             self.recv_data, addr = server_socket.recvfrom(256)
             #TODO if recv_data is a valid string -- do whatever
-            #server_socket.sendto(results, addr)
+            self.parse_input()
+            if self.rvalue:
+                server_socket.sendto(hex(rValue), addr)
+                self.rValue = None
             #
         self.server_socket.close()
     
@@ -38,25 +42,59 @@ class Make_server():
             to_call[m.group(1)](m.group(2))
         
         else:
+            #regular expression from http://docutils.sourceforge.net/docutils/utils/roman.py
             re1='(m)'
-            re2='(?:[M,D,C,L,X,V,I][M,D,C,L,X,V,I]+)' 
+            re2='M{0,4}'                # thousands - 0 to 4 M's
+            re3='(CM|CD|D?C{0,3})'      # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
+                                        #     or 500-800 (D, followed by 0 to 3 C's)
+            re4='(XC|XL|L?X{0,3})'      # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
+                                        #     or 50-80 (L, followed by 0 to 3 X's)
+            re5='(IX|IV|V?I{0,3})'      # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
+                                        #     or 5-8 (V, followed by 0 to 3 I's)
         
-            rg = re.compile(re1+re2,re.IGNORECASE|re.DOTALL)
+            rg = re.compile(re1+re2+re3+re4+re5,re.IGNORECASE|re.DOTALL)
             m = rg.search(self.recv_data)
             if m:
                 r_number(m.group(2))
-        
+        # else i don't care because its not a valid input
         
     def f_number(self, number):
         #given a decimal n 0 -300 return the nth fibonacci number in hex
+        if number >= 0 and number <=300:
+            from math import sqrt
+            self.rValue = ((1+sqrt(5))**number-(1-sqrt(5))**number)/(2**number*sqrt(5))
+                #it's math -- sometimes its ugly
+                
         
-    def d_number(self):
+    def d_number(self, number):
         #given a decimal number 0 - 10^30 (inclusive) return number in hex
-    
+        if number >=0 and number <=10**30:
+            self.rValue = number
+            
     def r_number(self):
         #given a roman numeral between I and MMMM (inclusive) return number in hex
-    
-    def  
+        # shamelessly taken from http://docutils.sourceforge.net/docutils/utils/roman.py
+        romanNumeralMap = (('M',  1000),
+                   ('CM', 900),
+                   ('D',  500),
+                   ('CD', 400),
+                   ('C',  100),
+                   ('XC', 90),
+                   ('L',  50),
+                   ('XL', 40),
+                   ('X',  10),
+                   ('IX', 9),
+                   ('V',  5),
+                   ('IV', 4),
+                   ('I',  1))
+
+        result = 0
+        index = 0
+        for numeral, integer in romanNumeralMap:
+            while s[index:index+len(numeral)] == numeral:
+                result += integer
+                index += len(numeral)
+        self.rValue=result
     
     
 def main():
@@ -64,6 +102,7 @@ def main():
     
     # make three threads as servers  
         # ports UID, UID + 1000, UID + 2000
+    # listen for user input -- if quit -- interrupt threads set quit flag and join 
 
 if __name__ == '__main__':
     main()
